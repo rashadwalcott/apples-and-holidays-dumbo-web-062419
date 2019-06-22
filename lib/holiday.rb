30ebfbb9bb418c25649ffea246581311313b9032
Rashad Walcott <rashad.walcott15@gmail.com>
2019-06-22 23:07:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -92,7 +92,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday.each do |name,supply|
       if supply == "BBQ"
-        puts "#{name.values}"
+        puts "#{name}"
       end
     end
   end
